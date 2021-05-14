51e459034d20abcd8a358773dd6b4251024cacec
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-05-14 10:37:50 +0900

    02. 변수

---
+++ 1장 JavaScript 입문/02. 변수.js
@@ -0,0 +1,19 @@
+let value = 1;
+console.log(value);
+value = 2;
+console.log(value);
+
+const a = 1;
+// a = 2;
+
+let text = 'hello';
+let name = '헬로우 자바스크립트';
+
+let good = true;
+let loading = false;
+
+let bad = null; // 진짜 없다
+let something = undefined; // 아직 정해지지 않았다
+
+let criminal;
+console.log(criminal);
