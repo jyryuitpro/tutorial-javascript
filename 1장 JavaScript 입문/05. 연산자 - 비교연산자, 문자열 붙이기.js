3da551f35bd78c328e48d298e040606eec450945
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-05-14 11:04:09 +0900

    05. 연산자 - 비교연산자, 문자열 붙이기

---
+++ 1장 JavaScript 입문/05. 연산자 - 비교연산자, 문자열 붙이기.js
@@ -0,0 +1,20 @@
+const a = 1;
+const b = '1';
+// const equals = a == b;
+// const equals = a != b;
+// const equals = a === b;
+const equals = a !== b;
+console.log(equals);
+
+const c = 10;
+const d = 15;
+const e = 15;
+
+console.log(c < d);
+console.log(c > d);
+console.log(d >= a);
+console.log(d <= a);
+
+const f = '안녕';
+const g = '하세요';
+console.log(f + g);
