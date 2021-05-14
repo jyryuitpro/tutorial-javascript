e6403e7d3c416720051864d1f0caddadda06122c
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-05-14 11:27:49 +0900

    07. 조건문 - switch case

---
+++ 1장 JavaScript 입문/07. 조건문 - switch case.js
@@ -0,0 +1,15 @@
+const device = 'iphone';
+
+switch (device) {
+    case 'iphone':
+        console.log('아이폰!');
+        break;
+    case 'ipad':
+        console.log('아이패트!');
+        break;
+    case 'galaxy note':
+        console.log('갤럭시 노트!');
+        break;
+    default:
+        console.log('모르겠네요..');
+}
