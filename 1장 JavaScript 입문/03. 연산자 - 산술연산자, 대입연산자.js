f4384e32886a3f685fbbbfd4e5aca2bc4b9dee7b
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-05-14 10:38:07 +0900

    03. 연산자 - 산술연산자, 대입연산자

---
+++ 1장 JavaScript 입문/03. 연산자 - 산술연산자, 대입연산자.js
@@ -0,0 +1,20 @@
+let value = 1;
+
+let a = 1 + 2 - (3 * 4) / 4;
+console.log(a);
+
+let b = 1;
+console.log(b++);
+console.log(b);
+console.log(++b);
+
+console.log(a--);
+console.log(a);
+
+let c = 1;
+c += 1;
+c += 3;
+c -= 3;
+c *= 3;
+c /= 3;
+console.log(c);
