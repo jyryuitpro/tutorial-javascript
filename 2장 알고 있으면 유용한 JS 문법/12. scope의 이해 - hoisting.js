67f7a2da562f5673b0a7b871aa395f8d7b324503
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-05-16 09:34:51 +0900

    12. scope의 이해 - hoisting

---
+++ 2장 알고 있으면 유용한 JS 문법/12. scope의 이해 - hoisting.js
@@ -0,0 +1,25 @@
+myFunction();
+
+function myFunction() {
+    console.log('hello world');
+}
+
+console.log(number);
+var number = 2;
+
+// var number;
+// console.log(number);
+// number = 2;
+
+function fn() {
+    console.log(a);
+    var a = 2;
+    // let a = 2;
+    // const a = 2;
+}
+
+fn();
+
+const myFunction = function myFunction() {
+    console.log('hello world');
+};
